f5f530e3a5c4a8c8edb9410f956cb015165e56f4
chilusoft <mukukachilu@gmail.com>
2020-09-16 03:46:49 -0700

    testing layer decoupling

--- index/views.py
+++ index/views.py
@@ -1,18 +1,34 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.http import HttpResponse, Http404
+from django.template import loader
 
 from .models import Question
 
 # Create your views here.
 
 def index(request):
+	'''
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	output = '<br>'.join([q.question_text + ' ' + str(q.pub_date) for q in latest_question_list])
-
-	return(HttpResponse(output))
+	#output = '<br>'.join([q.question_text + ' ' + str(q.pub_date) for q in latest_question_list])
+	context = { 'latest_question_list': latest_question_list}
+	#return(render(request, 'index/index.html', context))
+	return(HttpResponse(loader.get_template('index/index.html').render(context, request)))
+	'''
+	latest_question_list = get_list_or_404(Question)
+	context = { 'latest_question_list': latest_question_list}
+			
+	return(HttpResponse(loader.get_template('index/index.html').render(context, request)))
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+	'''
+	try:
+		question = Question.objects.get(pk=question_id)
+	except Question.DoesNotExist:
+		raise Http404('Question with id %s does not exist.' % question_id)
+	return(HttpResponse(loader.get_template('index/detail.html').render({'question': question}, request)))
+	'''
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'index/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
